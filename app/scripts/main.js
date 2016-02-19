88713ad0b8a8735e4b18a5825227538448d0c515
Evan Yamanishi <sh0ji@users.noreply.github.com>
2016-02-19 17:22:45 -0500

    Started NEDKit demo, now with more cats!

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,75 @@
-console.log('\'Allo \'Allo!');
+/******************
+* BS FUNCTIONS
+******************/
+$(function () {
+	$('[data-toggle="tooltip"]').tooltip();
+	$('[data-toggle="popover"]').popover();
+});
+
+/******************
+* IMAGE EXPANDER
+******************/
+$('.expander-click').on('click', function() {
+	var $expander = $(this).parent('.expander');
+	var $icon = $(this).find('.glyphicon');
+
+	$expander.swapClass('expander-collapsed', 'expander-expanded');
+	$icon.swapClass('glyphicon-menu-down', 'glyphicon-menu-up');
+})
+
+$.fn.swapClass = function (oldClass, newClass) {
+	$(this).toggleClass(oldClass).toggleClass(newClass);
+}
+
+/******************
+* TOGGLE BUTTONS
+******************/
+$('#pageNumbers').on('click', function() {
+	$('.pagebreak').toggle();
+	$('.naToggle').swapClass('col-sm-9 col-md-7', 'col-sm-10 col-md-8');
+	$(this).toggleClass('inactive');
+})
+
+$('#annotations').on('click', function() {
+	// $('[data-toggle="tooltip"]').tooltip('disable').toggleClass('tipped');
+	$(this).toggleClass('inactive');
+})
+
+
+/******************
+* Replace all SVG images with inline SVG
+* http://stackoverflow.com/questions/24933430/img-src-svg-changing-the-fill-color
+******************/
+jQuery('img.svg').each(function(){
+	var $img = jQuery(this);
+	var imgID = $img.attr('id');
+	var imgClass = $img.attr('class');
+	var imgURL = $img.attr('src');
+
+	jQuery.get(imgURL, function(data) {
+        // Get the SVG tag, ignore the rest
+        var $svg = jQuery(data).find('svg');
+
+        // Add replaced image's ID to the new SVG
+        if(typeof imgID !== 'undefined') {
+        	$svg = $svg.attr('id', imgID);
+        }
+        // Add replaced image's classes to the new SVG
+        if(typeof imgClass !== 'undefined') {
+        	$svg = $svg.attr('class', imgClass+' replaced-svg');
+        }
+
+        // Remove any invalid XML tags as per http://validator.w3.org
+        $svg = $svg.removeAttr('xmlns:a');
+
+        // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+        if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+        	$svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+        }
+
+        // Replace image with new SVG
+        $img.replaceWith($svg);
+
+    }, 'xml');
+
+});
