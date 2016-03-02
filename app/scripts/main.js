97707b23217413ddd5cd261766d27e04bbeaefdf
Evan Yamanishi <sh0ji@users.noreply.github.com>
2016-03-02 15:34:41 -0500

    cleanup and tooltip/popover fixes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,84 +1,90 @@
 'use strict';
 
 /******************
-* INITIALIZE!
-******************/
+ * INITIALIZE!
+ ******************/
 $(function () {
-	$('[data-toggle="tooltip"]').tooltip();
-	$('[data-toggle="popover"]').popover();
-	$('[data-toggle="photoswipe"]').photoswipe({
-		bgOpacity: 0.7
-	});
+  $('[data-toggle="tooltip"]').tooltip();
+  $('a[data-toggle="popover"]').popover();
+  
+  // popover tooltips as definition lists
+  $('dfn[data-toggle="popover"]').popover({
+    template: '<dl class="popover" role="tooltip"><div class="arrow"></div><dt class="popover-title"></dt><dd class="popover-content"></dd></dl>'
+  });
+  
+  $('[data-toggle="photoswipe"]').photoswipe({
+    bgOpacity: 0.7
+  });
 });
 
 /******************
-* IMAGE EXPANDER
-******************/
-$('.expander-click').click(function() {
-	var $expander = $(this).parent('.expander');
-	var $icon = $(this).find('.glyphicon');
+ * IMAGE EXPANDER
+ ******************/
+$('.expander-click').click(function () {
+  var $expander = $(this).parent('.expander');
+  var $icon = $(this).find('.glyphicon');
 
-	$expander.swapClass('expander-collapsed', 'expander-expanded');
-	$icon.swapClass('glyphicon-menu-down', 'glyphicon-menu-up');
+  $expander.swapClass('expander-collapsed', 'expander-expanded');
+  $icon.swapClass('glyphicon-menu-down', 'glyphicon-menu-up');
 });
 
 $.fn.swapClass = function (oldClass, newClass) {
-	$(this).toggleClass(oldClass).toggleClass(newClass);
+  $(this).toggleClass(oldClass).toggleClass(newClass);
 };
 
 /******************
-* TOGGLE BUTTONS
-******************/
-$('#pageNumbers').click(function() {
-	$('.pagebreak').toggle();
-	$('.naToggle').swapClass('col-sm-9 col-md-7', 'col-sm-10 col-md-8');
-	$(this).toggleClass('inactive');
+ * TOGGLE BUTTONS
+ ******************/
+$('#pageNumbers').click(function () {
+  $('.pagebreak').toggle();
+  $('.naToggle').swapClass('col-sm-9 col-md-7', 'col-sm-10 col-md-8');
+  $(this).toggleClass('inactive');
 });
 
-$('#annotations').click(function() {
-	$(this).toggleClass('inactive');
-	$('[data-toggle="tooltip"]').toggleClass('tipped');
-	if ($(this).hasClass('inactive')) {
-		$('[data-toggle="tooltip"]').tooltip('disable').removeAttr('tabindex');
-	} else {
-		$('[data-toggle="tooltip"]').tooltip('enable').attr('tabindex', 0);
-	}
+$('#annotations').click(function () {
+  $(this).toggleClass('inactive');
+  $('[data-toggle="tooltip"]').toggleClass('tipped');
+  if ($(this).hasClass('inactive')) {
+    $('[data-toggle="tooltip"]').tooltip('disable').removeAttr('tabindex');
+  } else {
+    $('[data-toggle="tooltip"]').tooltip('enable').attr('tabindex', 0);
+  }
 });
 
 /******************
-* Replace all SVG images with inline SVG
-* http://stackoverflow.com/questions/24933430/img-src-svg-changing-the-fill-color
-******************/
-jQuery('img.svg').each(function(){
-	var $img = jQuery(this);
-	var imgID = $img.attr('id');
-	var imgClass = $img.attr('class');
-	var imgURL = $img.attr('src');
+ * Replace all SVG images with inline SVG
+ * http://stackoverflow.com/questions/24933430/img-src-svg-changing-the-fill-color
+ ******************/
+jQuery('img.svg').each(function () {
+  var $img = jQuery(this);
+  var imgID = $img.attr('id');
+  var imgClass = $img.attr('class');
+  var imgURL = $img.attr('src');
 
-	jQuery.get(imgURL, function(data) {
-        // Get the SVG tag, ignore the rest
-        var $svg = jQuery(data).find('svg');
+  jQuery.get(imgURL, function (data) {
+    // Get the SVG tag, ignore the rest
+    var $svg = jQuery(data).find('svg');
 
-        // Add replaced image's ID to the new SVG
-        if(typeof imgID !== 'undefined') {
-			$svg = $svg.attr('id', imgID);
-        }
-        // Add replaced image's classes to the new SVG
-        if(typeof imgClass !== 'undefined') {
-			$svg = $svg.attr('class', imgClass + ' replaced-svg');
-        }
+    // Add replaced image's ID to the new SVG
+    if (typeof imgID !== 'undefined') {
+      $svg = $svg.attr('id', imgID);
+    }
+    // Add replaced image's classes to the new SVG
+    if (typeof imgClass !== 'undefined') {
+      $svg = $svg.attr('class', imgClass + ' replaced-svg');
+    }
 
-        // Remove any invalid XML tags as per http://validator.w3.org
-        $svg = $svg.removeAttr('xmlns:a');
+    // Remove any invalid XML tags as per http://validator.w3.org
+    $svg = $svg.removeAttr('xmlns:a');
 
-        // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
-        if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-			$svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'));
-        }
+    // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+    if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+      $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'));
+    }
 
-        // Replace image with new SVG
-        $img.replaceWith($svg);
+    // Replace image with new SVG
+    $img.replaceWith($svg);
 
-    }, 'xml');
+  }, 'xml');
 
 });
