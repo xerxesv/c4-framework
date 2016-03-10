8d2d3747c2386f316282cd0b0364efd379eba314
Evan Yamanishi <sh0ji@users.noreply.github.com>
2016-03-10 12:15:03 -0500

    photoswipe cleanup and customization

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -146,15 +146,12 @@ gulp.task('serve:test', ['scripts'], () => {
 // inject bower components
 gulp.task('wiredep', () => {
   gulp.src('app/styles/*.scss')
-    .pipe(wiredep({
-      ignorePath: /^(\.\.\/)+/
-    }))
+    .pipe(wiredep())
     .pipe(gulp.dest('app/styles'));
 
   gulp.src('app/**/*.html')
     .pipe(wiredep({
-      exclude: ['bootstrap-sass'],
-      ignorePath: /^(\.\.\/)+/
+      exclude: ['bootstrap-sass']
     }))
     .pipe(gulp.dest('app'));
 });
